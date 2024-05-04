e30b1f07881fe1d4db95d9792f86c9ec00109a8b
onlysits <168887155+onlysits@users.noreply.github.com>
2024-05-03 22:48:01 -0700

    new game

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
-// No JavaScript needed for this simple animation
+let mood = "Happy";
+let health = "Good";
+
+function feed() {
+    mood = "Happy";
+    updateStatus();
+}
+
+function play() {
+    mood = "Excited";
+    updateStatus();
+}
+
+function clean() {
+    health = "Good";
+    updateStatus();
+}
+
+function updateStatus() {
+    document.getElementById("mood").innerText = "Mood: " + mood;
+    document.getElementById("health").innerText = "Health: " + health;
+}
